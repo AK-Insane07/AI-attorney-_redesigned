1fd4bd4c77c4d5c6bd1efed3283120aee362cadf
AK-Insane07 <127690957+AK-Insane07@users.noreply.github.com>
2024-07-13 18:06:19 +0530

    added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        cormorantgaramond:['Cormorant-Garamond','Roboto'],
+      },
+
+    },
+  },
+  plugins: [],
+}
+
